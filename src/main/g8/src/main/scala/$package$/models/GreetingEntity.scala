7554d3ceb8c3166e2e93dafc62a8622386b7e69f
orhanbalci <orhanbalci@gmail.com>
2017-03-07 10:01:50 +0200

    slick fix

--- src/main/g8/src/main/scala/$package$/models/GreetingEntity.scala
+++ src/main/g8/src/main/scala/$package$/models/GreetingEntity.scala
@@ -1,9 +1,9 @@
 package $package$.models
-import slick.driver.H2Driver.api._ 
+import slick.driver.H2Profile.api._ 
 
 case class GreetingEntity(id: Int, greeting: String)
 
-class Greetings(tag : Tag) extends Table[(Int, String)](tag, "GREETINGS"){
+class Greetings(tag : Tag) extends Table[GreetingEntity](tag, "GREETINGS"){
   def id = column[Int]("id",O.PrimaryKey)
   def greeting = column[String]("greeting")
   def * = (id, greeting) <> (GreetingEntity.tupled, GreetingEntity.unapply)
